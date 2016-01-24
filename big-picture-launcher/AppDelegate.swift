c0c02287d000ae5dbc65bbecbcfaea171b91d154
Qasim Iqbal <me@qas.im>
2016-01-24 12:47:10 -0400

    Xcode init

---
+++ big-picture-launcher/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  big-picture-launcher
+//
+//  Created by Qasim on 2016-01-28.
+//  Copyright © 2016 Qasim Iqbal. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
