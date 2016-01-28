f9de296f6e5b3d3ab5ef8f75c612d47785429f04
Qasim Iqbal <me@qas.im>
2016-01-28 12:46:26 -0500

    Listen for controller connection (not tested)

--- big-picture-launcher/AppDelegate.swift
+++ big-picture-launcher/AppDelegate.swift
@@ -13,14 +13,22 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
-
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+
+        NSNotificationCenter.defaultCenter().addObserver(
+            self,
+            selector: "controllerDidConnect",
+            name: "GCControllerDidConnectNotification",
+            object: nil)
+    }
+
+    func controllerDidConnect() {
+        // TODO: Launch Big Picture
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
-
 
 }
